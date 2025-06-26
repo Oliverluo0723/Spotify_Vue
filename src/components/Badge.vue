998de377f1cd4c3c068a2a7fc0198c8d7ef25938
BD <oliverluo0723@gmail.com>
2025-06-27 02:33:57 +0800

    feat: 封裝Badge組件

---
+++ src/components/Badge.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { theme } from 'ant-design-vue'
+
+const { token } = theme.useToken()
+
+const bgPrimary = token.value.colorPrimary
+
+console.log(bgPrimary)
+</script>
+<template>
+  <div class="badge" :style="{ backgroundColor: bgPrimary }"><slot></slot></div>
+</template>
+<style lang="scss" scoped>
+.badge {
+  width: fit-content;
+  height: fit-content;
+  padding: 2px 8px;
+  font-size: 0.65rem;
+  color: #ededed;
+  border-radius: 12px;
+}
+</style>
