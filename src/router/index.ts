8286a8079364656a576e9e92c33121215e9a84ea
BD <oliverluo0723@gmail.com>
2025-06-26 19:45:43 +0800

    fix(router): 修正路由配置，重定向至首頁

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 
 const router = createRouter({
+  // vite.config.ts 中的環境變量 import.meta.env.BASE_URL
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home,
     },
