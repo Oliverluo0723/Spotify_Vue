3747114db24d2759f9493f13cd72577ad0128849
BD <oliverluo0723@gmail.com>
2025-06-26 20:18:38 +0800

    feat(ui)配置ant-desing組件庫 及全局配置項目ConfigProvider

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+import { ConfigProvider, Button, theme } from 'ant-design-vue'
+
+const defaultData = {
+  borderRadius: 6,
+  colorPrimary: '#3c3c3c',
+}
+
+const data = ref(defaultData)
+
+const { token } = theme.useToken()
 </script>
 
 <template>
-  <div class="container">
-    <h2>我是App</h2>
+  <ConfigProvider
+    :theme="{
+      token: {
+        colorPrimary: data.colorPrimary,
+        borderRadius: data.borderRadius,
+      },
+    }"
+  >
     <RouterView />
-  </div>
+  </ConfigProvider>
 </template>
 
-<style scoped lang="scss">
-.container {
-  width: 100vw;
-  height: auto;
-}
-</style>
+<style scoped lang="scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+  ],
   base: '/jd',
   resolve: {
     alias: {
