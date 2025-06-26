e654329551c4f62543e8ae87f73b5824866a2559
BD <oliverluo0723@gmail.com>
2025-06-26 19:19:58 +0800

    chore: 初始化項目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <h2>我是App</h2>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <h1>我是Home</h1>
+</template>
+
+<style scoped></style>
