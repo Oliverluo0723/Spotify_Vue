866f5209e0187c3dff7d7d16a7f8a862f6f3f450
BD <oliverluo0723@gmail.com>
2025-06-27 17:34:53 +0800

    feat: Header組件更新為RWD

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { h } from 'vue'
+import { h, ref } from 'vue'
 // UI
 import { Input } from 'ant-design-vue'
 // icon
@@ -14,6 +14,14 @@ import {
 import Spotify from '@/assets/spotify-1.svg'
 
 const SpotifyIcon = Spotify
+
+const menuItems = ref([
+  { id: 101, menuItem: '帳戶', Url: '' },
+  { id: 102, menuItem: '個人檔案', Url: '' },
+  { id: 103, menuItem: '支援', Url: '' },
+  { id: 104, menuItem: '下載', Url: '' },
+  { id: 105, menuItem: '設定', Url: '' },
+])
 </script>
 <template>
   <header>
@@ -25,18 +33,39 @@ const SpotifyIcon = Spotify
         <template #title>首頁</template>
         <a-button type="primary" shape="circle" :icon="h(HomeFilled)"></a-button>
       </a-tooltip>
-      <a-input style="width: 250px" placeholder="想聽" :suffix="h(CustomerServiceOutlined)">
+      <a-input
+        style="width: clamp(10rem, 35vw, 20rem)"
+        placeholder="想聽什麼呢"
+        :suffix="h(CustomerServiceOutlined)"
+      >
         <template #prefix>
           <SearchOutlined />
         </template>
       </a-input>
     </div>
     <div class="right">
-      <BellOutlined />
-      <TeamOutlined />
-      <a-avatar>
-        <template #icon><UserOutlined /></template>
-      </a-avatar>
+      <!-- 通知 -->
+      <a-tooltip>
+        <template #title>通知</template>
+        <BellOutlined />
+      </a-tooltip>
+      <!-- 好友活動 -->
+      <a-tooltip>
+        <template #title>好友活動</template>
+        <TeamOutlined />
+      </a-tooltip>
+      <a-dropdown>
+        <a-avatar>
+          <template #icon><UserOutlined /></template>
+        </a-avatar>
+        <template #overlay>
+          <a-menu>
+            <a-menu-item v-for="item in menuItems" :key="item.id">
+              <a :href="item.Url">{{ item.menuItem }}</a>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
     </div>
   </header>
 </template>
