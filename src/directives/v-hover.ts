4e82ebf26c7d98bdaa336ce9203332e7d4bcc9ec
BD <oliverluo0723@gmail.com>
2025-06-30 04:09:19 +0800

    fix: 修正v-hover高度限制問題

--- src/directives/v-hover.ts
+++ src/directives/v-hover.ts
@@ -3,6 +3,7 @@ import type { Directive } from 'vue'
 
 export const hover: Directive = {
   mounted(el) {
+    // hover-effect 在 global.css裡面
     el.classList.add('hover-effect')
   },
   unmounted(el) {
--- src/views/LibrarySection/components/libraryList.vue
+++ src/views/LibrarySection/components/libraryList.vue
@@ -10,7 +10,7 @@ const artistData = ref(mockArtists)
     <ul class="w-full">
       <li v-for="item in artistData" :key="item.id" class="mt-3 relative z-10">
         <RouterLink :to="`/home/aritist${item.id}`">
-          <div class="isHover">
+          <div v-hover>
             <a-flex align="center" gap="12">
               <div>
                 <a-avatar :src="item.images[0].url"></a-avatar>
@@ -26,16 +26,4 @@ const artistData = ref(mockArtists)
     </ul>
   </section>
 </template>
-<style lang="scss" scoped>
-.isHover:hover::before {
-  content: '';
-  width: 100%;
-  height: 40px;
-  position: absolute;
-  inset: 0 0 0 0;
-  opacity: 0.2;
-  border-radius: 8px;
-  background-color: #3c3c3c;
-  z-index: 0;
-}
-</style>
+<style lang="scss" scoped></style>
